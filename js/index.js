d88016ed9924bcb4579f165111f50825d4023143
zhangyinping <zhangyinping@123.com>
2017-10-25 15:47:49 +0800

    项目

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lenovo on 2017/10/9.
+ */
+$(function(){
+    $.stellar({
+        //横向无视差滚动效果
+        horizontalScrolling:false,
+        //页面尺寸发生改变重新渲染页面
+        responsive: true
+    });
+});
